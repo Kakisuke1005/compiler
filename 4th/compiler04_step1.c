b40c8c87b581976792c834518b53c8ca01268cdd
Kakisuke1005 <n1005kjiam@gmai.com>
2020-10-14 17:50:49 +0900

    ちかれた

--- 4th/compiler04_step1.c
+++ 4th/compiler04_step1.c
@@ -8,6 +8,34 @@ int nextChar(void);
 
 FILE *fp;
 
-int main(int argc,char *argv[]){}
+int main(int argc,char *argv[]){
 
-int nextChar(void){}
+	if(argc<2){
+		printf("no file name\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if((fp=fopen(argv[1],"r"))==NULL){
+		printf("error: file %s can not open\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	int ch;
+	while((ch=nextChar())!=EOF){
+		printf("%c,", (char)ch);
+	}
+	printf("\n");
+
+	fclose(fp);
+
+	return 0;
+}
+
+int nextChar(void){
+	static int ch;
+	if(ch==EOF){
+		return ch;
+	}
+	ch=fgetc(fp);
+	return ch;
+}
